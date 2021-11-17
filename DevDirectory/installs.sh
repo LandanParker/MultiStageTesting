5d7c8c20c162420d4ccbefbfa2206a7253fab601
EpicMice <micecd@gmail.com>
2021-11-17 02:19:07 -0600

    adding more initial files

---
+++ DevDirectory/installs.sh
@@ -0,0 +1,13 @@
+apt-get install -y nano; /
+apt-get  install -y net-tools; /
+apt-get  install -y wget; /
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb; /
+dpkg -i packages-microsoft-prod.deb; /
+apt-get update; /
+apt-get  install -y apt-transport-https; /
+apt-get  install -y dotnet-sdk-5.0; /
+apt-get  install -y dotnet-runtime-5.0; /
+apt-get  install -y dotnet-sdk-3.1; /
+apt-get  install -y dotnet-runtime-3.1; /
+apt-get  install -y aspnetcore-runtime-5.0; /
+apt-get  install -y aspnetcore-runtime-3.1;
