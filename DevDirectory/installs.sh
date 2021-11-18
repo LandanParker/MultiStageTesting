5ab71697b05971e8472ae491438db49cca34485f
EpicMice <micecd@gmail.com>
2021-11-17 19:21:42 -0600

    modified startup to have dotnet 6

--- DevDirectory/installs.sh
+++ DevDirectory/installs.sh
@@ -5,9 +5,12 @@ wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.
 dpkg -i packages-microsoft-prod.deb; /
 apt-get update; /
 apt-get  install -y apt-transport-https; /
+apt-get  install -y dotnet-sdk-6.0; /
+apt-get  install -y dotnet-runtime-6.0; /
+apt-get  install -y aspnetcore-runtime-6.0; /
 apt-get  install -y dotnet-sdk-5.0; /
 apt-get  install -y dotnet-runtime-5.0; /
+apt-get  install -y aspnetcore-runtime-5.0; /
 apt-get  install -y dotnet-sdk-3.1; /
 apt-get  install -y dotnet-runtime-3.1; /
-apt-get  install -y aspnetcore-runtime-5.0; /
 apt-get  install -y aspnetcore-runtime-3.1;
